9ba537f9583b07af3912a42e6d4a2402bf9f4713
James Hunter <james.hunter@me.com>
2024-12-16 15:09:17 +0000

    Day 13b

--- days/day13/day13.ts
+++ days/day13/day13.ts
@@ -38,3 +38,28 @@ export function run(input: string): number {
   return machines.reduce((total, machine) => total + calculateMachineTokens(machine), 0);
 }
 
+const parseMachineAgain = (input: string): Machine => {
+  const [buttonAString, buttonBString, prizeString] = input.split('\n');
+  // biome-ignore lint/style/noNonNullAssertion: It's fine
+  const [,buttonAX, buttonAY] = buttonAString.match(/.+X\+(\d+), Y\+(\d+)/)!;
+  // biome-ignore lint/style/noNonNullAssertion: It's fine
+  const [,buttonBX, buttonBY] = buttonBString.match(/.+X\+(\d+), Y\+(\d+)/)!;
+  // biome-ignore lint/style/noNonNullAssertion: It's fine
+  const [,prizeX, prizeY] = prizeString.match(/.+X=(\d+), Y=(\d+)/)!
+  
+  return {
+    a: { x: Number.parseInt(buttonAX), y: Number.parseInt(buttonAY) },
+    b: { x: Number.parseInt(buttonBX), y: Number.parseInt(buttonBY) },
+    prize: { x: Number.parseInt(prizeX) + 10000000000000, y: Number.parseInt(prizeY) + 10000000000000 }
+  }
+}
+
+const parseInputAgain = (input: string) => {
+  return input.split('\n\n').map(parseMachineAgain);
+}
+
+export function runAgain(input: string): number {
+  const machines = parseInputAgain(input);
+  return machines.reduce((total, machine) => total + calculateMachineTokens(machine), 0);
+}
+
