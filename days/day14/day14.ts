9ee75011244ecc2df9d7021d4946ee75a8867578
James Hunter <james.hunter@me.com>
2024-12-16 20:57:09 +0000

    Day 14a

---
+++ days/day14/day14.ts
@@ -0,0 +1,65 @@
+type Point = { x: number; y: number };
+type Velocity = Point;
+
+const parseInput = (input: string): Array<{ point: Point; velocity: Velocity }> => {
+  return input.split('\n').map((line) => {
+    // biome-ignore lint/style/noNonNullAssertion: It's fine
+    const [, pointX, pointY, velocityX, velocityY] = line.match(/p=(.+),(.+) v=(.+),(.+)/)!;
+    return {
+      point: { x: Number.parseInt(pointX), y: Number.parseInt(pointY) },
+      velocity: { x: Number.parseInt(velocityX), y: Number.parseInt(velocityY) }
+    };
+  });
+};
+
+const runTheNumbers = ({
+  bots,
+  gridDimensions,
+  iterations
+}: {
+  bots: Array<{ point: Point; velocity: Velocity }>;
+  gridDimensions: { width: number; height: number };
+  iterations: number;
+}): Array<Point> => {
+  return bots.map((bot) => {
+    const furthestX = bot.point.x + bot.velocity.x * iterations;
+    const furthestY = bot.point.y + bot.velocity.y * iterations;
+    return {
+      x: (gridDimensions.width + (furthestX % gridDimensions.width)) % gridDimensions.width,
+      y: (gridDimensions.height + (furthestY % gridDimensions.height)) % gridDimensions.height
+    };
+  });
+};
+
+const quadrantOfBot = (bot: Point, gridDimensions: { width: number; height: number }): 1 | 2 | 3 | 4 | null => {
+  const midX = Math.floor(gridDimensions.width / 2);
+  const midY = Math.floor(gridDimensions.height / 2);
+
+  if (bot.x === midX || bot.y === midY) return null;
+
+  if (bot.x < midX && bot.y < midY) return 1;
+  if (bot.x > midX && bot.y < midY) return 2;
+  if (bot.x < midX && bot.y > midY) return 3;
+  if (bot.x > midX && bot.y > midY) return 4;
+
+  return null;
+};
+
+const scoreResults = ({
+  bots,
+  gridDimensions
+}: { bots: Array<Point>; gridDimensions: { width: number; height: number } }): number => {
+  const scores = { 1: 0, 2: 0, 3: 0, 4: 0 };
+  for (const bot of bots) {
+    const quadrant = quadrantOfBot(bot, gridDimensions);
+    if (quadrant !== null) scores[quadrant] += 1;
+  }
+  return scores[1] * scores[2] * scores[3] * scores[4];
+};
+
+export function run(input: string, gridDimensions: { width: number; height: number }): number {
+  const bots = parseInput(input);
+  const movedBots = runTheNumbers({ bots, gridDimensions, iterations: 100 });
+
+  return scoreResults({ bots: movedBots, gridDimensions });
+}
