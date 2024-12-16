8abbcd08ba12fb5b3876dcf03b31b33c7380a722
James Hunter <james.hunter@me.com>
2024-12-16 14:59:00 +0000

    Day 13a

---
+++ days/day13/day13.ts
@@ -0,0 +1,40 @@
+interface Machine {
+  a: { x: number; y: number; }
+  b: { x: number; y: number; }
+  prize: { x: number; y: number; }
+}
+
+const parseMachine = (input: string): Machine => {
+  const [buttonAString, buttonBString, prizeString] = input.split('\n');
+  // biome-ignore lint/style/noNonNullAssertion: It's fine
+  const [,buttonAX, buttonAY] = buttonAString.match(/.+X\+(\d+), Y\+(\d+)/)!;
+  // biome-ignore lint/style/noNonNullAssertion: It's fine
+  const [,buttonBX, buttonBY] = buttonBString.match(/.+X\+(\d+), Y\+(\d+)/)!;
+  // biome-ignore lint/style/noNonNullAssertion: It's fine
+  const [,prizeX, prizeY] = prizeString.match(/.+X=(\d+), Y=(\d+)/)!
+  
+  return {
+    a: { x: Number.parseInt(buttonAX), y: Number.parseInt(buttonAY) },
+    b: { x: Number.parseInt(buttonBX), y: Number.parseInt(buttonBY) },
+    prize: { x: Number.parseInt(prizeX), y: Number.parseInt(prizeY) }
+  }
+}
+
+const parseInput = (input: string) => {
+  return input.split('\n\n').map(parseMachine);
+}
+
+const calculateMachineTokens = (machine: Machine): number => {
+  const aValue = ((machine.b.y * machine.prize.x) - (machine.b.x * machine.prize.y)) / ((machine.b.y * machine.a.x)  - (machine.b.x * machine.a.y));
+  const bValue = ((machine.a.y * machine.prize.x) - (machine.a.x * machine.prize.y)) / ((machine.a.y * machine.b.x)  - (machine.a.x * machine.b.y));
+
+  if (aValue % 1 !== 0 || bValue % 1 !== 0) return 0;
+
+  return (aValue * 3) + bValue;
+}
+
+export function run(input: string): number {
+  const machines = parseInput(input);
+  return machines.reduce((total, machine) => total + calculateMachineTokens(machine), 0);
+}
+
