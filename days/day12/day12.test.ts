84c03c725dcf25924341cdb97189486978f94357
James Hunter <james.hunter@me.com>
2024-12-16 11:37:45 +0000

    Day 12b

--- days/day12/day12.test.ts
+++ days/day12/day12.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from 'bun:test';
-import { run } from './day12';
+import { run, runAgain } from './day12';
 import day12Input from './day12.input';
 
 describe('Day 12', () => {
@@ -38,6 +38,63 @@ MMMISSJEEE`;
     test('real puzzle', () => {
       const result = run(day12Input);
       expect(result).toBe(1402544);
+    });
+  });
+
+  describe('part 2', () => {
+    test('test case', () => {
+      const input = `AAAA
+BBCD
+BBCC
+EEEC`;
+      expect(runAgain(input)).toBe(80);
+    });
+
+    test('test case 2', () => {
+      const input = `OOOOO
+OXOXO
+OOOOO
+OXOXO
+OOOOO`;
+      expect(runAgain(input)).toBe(436);
+    });
+
+    test('test case 3', () => {
+      const input = `EEEEE
+EXXXX
+EEEEE
+EXXXX
+EEEEE`;
+      expect(runAgain(input)).toBe(236);
+    });
+
+    test('test case 4', () => {
+      const input = `AAAAAA
+AAABBA
+AAABBA
+ABBAAA
+ABBAAA
+AAAAAA`;
+      expect(runAgain(input)).toBe(368);
+    });
+
+    test('test case 5', () => {
+      const input = `RRRRIICCFF
+RRRRIICCCF
+VVRRRCCFFF
+VVRCCCJFFF
+VVVVCJJCFE
+VVIVCCJJEE
+VVIIICJJEE
+MIIIIIJJEE
+MIIISIJEEE
+MMMISSJEEE`;
+      expect(runAgain(input)).toBe(1206);
+    });
+
+    test('real puzzle', () => {
+      const result = runAgain(day12Input);
+      expect(result).toBe(862486);
     });
   });
 });
--- days/day12/day12.ts
+++ days/day12/day12.ts
@@ -1,11 +1,13 @@
-type Point = { x: number; y: number; };
+type Point = { x: number; y: number };
 type StringPoint = `${number},${number}`;
+type Direction = 'N' | 'E' | 'S' | 'W';
+type ImmediateBorders = { N: Point | undefined; E: Point | undefined; S: Point | undefined; W: Point | undefined };
 
 const stringPoint = (point: Point): StringPoint => `${point.x},${point.y}`;
 const pointPoint = (point: StringPoint): Point => {
   const [x, y] = point.split(',');
   return { x: Number.parseInt(x), y: Number.parseInt(y) };
-}
+};
 
 class Plot {
   plot: Array<Array<string>>;
@@ -33,6 +35,18 @@ class Plot {
     return neighbours;
   }
 
+  borderSegmentsOf(point: Point): ImmediateBorders {
+    const targetPlant = this.plantAt(point);
+    const borders: ImmediateBorders = { N: undefined, E: undefined, S: undefined, W: undefined };
+
+    if (this.plot[point.y - 1]?.[point.x] !== targetPlant) borders.N = point;
+    if (this.plot[point.y + 1]?.[point.x] !== targetPlant) borders.S = { ...point, y: point.y + 1 };
+    if (this.plot[point.y]?.[point.x - 1] !== targetPlant) borders.W = point;
+    if (this.plot[point.y]?.[point.x + 1] !== targetPlant) borders.E = { ...point, x: point.x + 1 };
+
+    return borders;
+  }
+
   plantAt(point: Point) {
     return this.plot[point.y]?.[point.x];
   }
@@ -47,9 +61,9 @@ class Plot {
 }
 
 const parseInput = (input: string): Plot => {
-  const plot = input.split('\n').map(row => row.split(''));
+  const plot = input.split('\n').map((row) => row.split(''));
   return new Plot(plot);
-}
+};
 
 type Area = Set<StringPoint>;
 
@@ -61,13 +75,13 @@ const delveFurtherFrom = (point: Point, plot: Plot, area: Area) => {
     if (plot.plantAt(neighbour) !== targetPlant) continue;
     area.add(stringPoint(neighbour));
 
-    delveFurtherFrom(neighbour, plot, area)
+    delveFurtherFrom(neighbour, plot, area);
   }
-}
+};
 
 const findAreaFrom = (point: Point, plot: Plot) => {
   const area = new Set<StringPoint>([stringPoint(point)]);
-  
+
   delveFurtherFrom(point, plot, area);
 
   return area;
@@ -80,15 +94,15 @@ const findAreas = (plot: Plot) => {
     for (let x = 0; x < plot.width; x += 1) {
       const point = { x, y };
       const plant = plot.plantAt(point);
-      if (areas[plant]?.find(area => area.has(stringPoint(point)))) continue;
+      if (areas[plant]?.find((area) => area.has(stringPoint(point)))) continue;
       const area = findAreaFrom(point, plot);
       areas[plant] ||= [];
-      areas[plant].push(area)
+      areas[plant].push(area);
     }
   }
 
   return areas;
-}
+};
 
 const scoreArea = (area: Area, plot: Plot): number => {
   const size = area.size;
@@ -98,7 +112,7 @@ const scoreArea = (area: Area, plot: Plot): number => {
   }, 0);
 
   return size * perimeter;
-}
+};
 
 export function run(input: string): number {
   const plot = parseInput(input);
@@ -109,3 +123,62 @@ export function run(input: string): number {
   }, 0);
 }
 
+const scoreAreaAgain = (area: Area, plot: Plot): number => {
+  type Borders = Record<`${Direction}${number}`, Set<number>>;
+  const borderRecord: Borders = {};
+
+  for (const plant of area) {
+    const borderSegments = plot.borderSegmentsOf(pointPoint(plant));
+    if (borderSegments.N) {
+      const key = `N${borderSegments.N.y}` as const;
+      borderRecord[key] ||= new Set();
+      borderRecord[key].add(borderSegments.N.x);
+    }
+    if (borderSegments.S) {
+      const key = `S${borderSegments.S.y}` as const;
+      borderRecord[key] ||= new Set();
+      borderRecord[key].add(borderSegments.S.x);
+    }
+    if (borderSegments.E) {
+      const key = `E${borderSegments.E.x}` as const;
+      borderRecord[key] ||= new Set();
+      borderRecord[key].add(borderSegments.E.y);
+    }
+    if (borderSegments.W) {
+      const key = `W${borderSegments.W.x}` as const;
+      borderRecord[key] ||= new Set();
+      borderRecord[key].add(borderSegments.W.y);
+    }
+  }
+
+  const borders = Object.values(borderRecord).reduce((total, borderLocations) => {
+    const sortedBorders = [...borderLocations].sort((a, b) => a - b);
+    let localCount = 0;
+    sortedBorders.forEach((segment, index) => {
+      if (index === 0) {
+        localCount += 1;
+        return;
+      }
+      const previousSegment = sortedBorders[index - 1];
+      if (previousSegment !== segment - 1) {
+        localCount += 1;
+      }
+    });
+
+    return total + localCount;
+  }, 0);
+
+  const size = area.size;
+
+  return borders * size;
+};
+
+export function runAgain(input: string): number {
+  const plot = parseInput(input);
+  const areas = findAreas(plot);
+
+  return Object.values(areas).reduce((total, plantAreas) => {
+    const plantScore = plantAreas.reduce((subTotal, area) => subTotal + scoreAreaAgain(area, plot), 0);
+    return total + plantScore;
+  }, 0);
+}
